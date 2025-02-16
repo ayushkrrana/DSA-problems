ab462fadef79a9bd01cbf48b5179788fe4265517
Ayush Kumar <160728166+ayushkrrana@users.noreply.github.com>
2025-02-17 01:29:39 +0530

    Time: 2 ms (39.08%), Space: 8.6 MB (20.28%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,7 +5,7 @@ public:
         int reverse=0;
         while(n!=0){
             int last_digit=n%10;
-            if(reverse>INT_MAX/10 || reverse<INT_MIN/10){
+            if(reverse>INT_MAX/10 || reverse<INT_MIN/10){// we divide by 10 to check early to prevent overflow because int can only store upto 32 bit so we take one less digit by/10 to check
                 return 0;
             }
             n=n/10;
